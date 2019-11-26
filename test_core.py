8327ba3bdf20b789b28c9a11fee211bd4caceca7
Teemu Kalvas <teemu@montel.fi>
2019-11-26 14:53:44 +0200

    generators

--- test_core.py
+++ test_core.py
@@ -1,49 +1,28 @@
 import unittest
 import core
-
-def is_leap(yg):
-    return yg % 400 == 0 or yg % 4 == 0 and yg % 100 != 0
+import step
+from itertools import islice
 
 class TestDate(unittest.TestCase):
     def test_exhaustive(self):
-        dp = None
-        yg, mg, dg = 2001, 1, 1
-        yi, wi, di = 2001, 1, 1
-        weeks = 52
-        while yg < 2401:
-            d = core.from_gregorian(yg, mg, dg)
-            if dp is not None:
-                self.assertEqual(d, dp + 1)
-            self.assertEqual(core.to_gregorian(d), (yg, mg, dg))
-            self.assertEqual(core.from_iso_week_date(yi, wi, di), d)
-            self.assertEqual(core.to_iso_week_date(d), (yi, wi, di))
-            self.assertEqual(core.weekday(d) + 1, di)
-            dp = d
-            if mg == 2 and dg == 28:
-                if is_leap(yg):
-                    dg = 29
-                else:
-                    mg = 3
-                    dg = 1
-            else:
-                dg = dg + 1
-                if dg > [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][mg]:
-                    mg += 1
-                    dg = 1
-                if mg > 12:
-                    yg += 1
-                    mg = 1
-            di += 1
-            if di > 7:
-                wi += 1
-                di = 1
-            if wi > weeks:
-                yi += 1
-                wi = 1
-                if (mg, dg) == (12, 29) or (mg, dg) == (12, 30) and is_leap(yi):
-                    weeks = 53
-                else:
-                    weeks = 52
+        dp = 730790
+        for g, w, in islice(zip(step.generate_gregorian(dp + 1),
+                                step.generate_iso_week_date(dp + 1)),
+                            0, 146097):
+            self.assertEqual(g.day, dp + 1)
+            self.assertEqual(g.day, w.day)
+            self.assertEqual(g.weekday + 1, w.day_of_week)
+            self.assertEqual(core.to_gregorian(g.day),
+                             (g.year, g.month, g.day_of_month))
+            self.assertEqual(core.from_gregorian(g.year, g.month,
+                                                 g.day_of_month),
+                             g.day)
+            self.assertEqual(core.to_iso_week_date(w.day),
+                             (w.year, w.week, w.day_of_week))
+            self.assertEqual(core.from_iso_week_date(w.year, w.week,
+                                                     w.day_of_week),
+                             w.day)
+            dp = g.day
 
     def assertEaster(self, y, m, d):
         easter = core.easter(y)
